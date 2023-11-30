adfd64d89cbaedc11615dfab8dc8d20888c2dbb4
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2023-11-30 22:16:18 +0100

    Initial Commit

---
+++ CustomDynamicType/CustomDynamicType.h
@@ -0,0 +1,18 @@
+//
+//  CustomDynamicType.h
+//  CustomDynamicType
+//
+//  Created by Giuseppe Travasoni on 30/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomDynamicType.
+FOUNDATION_EXPORT double CustomDynamicTypeVersionNumber;
+
+//! Project version string for CustomDynamicType.
+FOUNDATION_EXPORT const unsigned char CustomDynamicTypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomDynamicType/PublicHeader.h>
+
+
