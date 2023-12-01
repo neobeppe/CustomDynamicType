82d0a99fde06d6c41777c297dbdaa73ef0cdde34
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2023-12-01 16:03:34 +0100

    improving SwiftUI Example

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -10,15 +10,18 @@ import CustomDynamicType
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("There is no strife, no prejudice, no national conflict in outer space as yet.")
-                .font(Font.font(style: .largeTitle, overrideWeight: .bold))
-            Text("Its hazards are hostile to us all.")
-                .font(Font.font(style: .headline))
-            Text("Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation many never come again.")
-            Text("But why, some say, the moon? Why choose this as our goal?")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 12.0) {
+                Text("There is no strife, no prejudice, no national conflict in outer space as yet.")
+                    .font(Font.font(style: .largeTitle, overrideWeight: .bold))
+                Text("Its hazards are hostile to us all.")
+                    .font(Font.font(style: .headline))
+                Text("Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation many never come again.")
+                    .font(Font.font(style: .title1))
+                Text("But why, some say, the moon? Why choose this as our goal?")
+                    .font(Font.font(style: .footnote))
+            }.padding()
         }
-        .padding()
     }
 }
 
