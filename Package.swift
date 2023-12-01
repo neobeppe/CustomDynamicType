d195129c52fb1060b1ae5d3ec7d6ecad3a0f06f4
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2023-12-01 22:55:24 +0100

    Package.swift file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CustomDynamicType",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "CustomDynamicType",
+            targets: ["CustomDynamicType"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "CustomDynamicType",
+            dependencies: [])
+    ]
+)
